b721e18e4c134174733534290be982fbdb5b5a10
zhongliang.zhou <zhongliang.zhou@kingsgroupgames.com>
2021-02-01 23:37:56 +0800

    配置好第一版xcode环境

--- LearnOpenGL/Learn/main.cpp
+++ LearnOpenGL/Learn/main.cpp
@@ -1,16 +1,45 @@
-//
-//  main.cpp
-//  Learn
-//
-//  Created by zhongliang.zhou on 2021/2/1.
-//
+#include <iostream>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+void Render(void)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    {
+        glColor3f(1.0,0.0,0.0);
+        glVertex2f(0, .5);
+        glColor3f(0.0,1.0,0.0);
+        glVertex2f(-.5,-.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(.5, -.5);
+    }
+    glEnd();
+}
+
 int main(int argc, const char * argv[]) {
-    glfwInit();
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLFWwindow* win;
+    if(!glfwInit()){
+        return -1;
+    }
+    win = glfwCreateWindow(640, 480, "OpenGL Base Project", NULL, NULL);
+    if(!win)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if(!glewInit())
+    {
+        return -1;
+    }
+    glfwMakeContextCurrent(win);
+    while(!glfwWindowShouldClose(win)){
+        Render();
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
     return 0;
 }
