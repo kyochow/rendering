bf9dc268029ca2b454e7fe2148ce1936a8da62b1
zhongliang.zhou <zhongliang.zhou@kingsgroupgames.com>
2021-02-02 20:52:40 +0800

    添加helloworld注释

--- LearnOpenGL/HelloWorld/main.cpp
+++ LearnOpenGL/HelloWorld/main.cpp
@@ -4,23 +4,31 @@
 
 int main(int argc, const char * argv[]) {
     GLFWwindow* win;
+    //初始化GLFW
     if(!glfwInit()){
         return -1;
     }
+    //创建Window
     win = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if(!win)
     {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    //初始化GLEW
     if(!glewInit())
     {
         return -1;
     }
     glfwMakeContextCurrent(win);
+    
+    //保持窗口打开状态
     while(!glfwWindowShouldClose(win)){
         glfwPollEvents();
     }
+    
+    //退出
     glfwTerminate();
     exit(EXIT_SUCCESS);
     return 0;
